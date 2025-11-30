19907e0fdbffd8d3925303958706e88efdb3681d
Mykal-Steele <mykal.stele@gmail.com>
2025-11-30 22:40:51 +0700

    format

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-    headers := http.Header{"Authorization": []string{"ApiKey testKey"}}
-    key, err := GetAPIKey(headers)
-    if err != nil {
-        t.Errorf("expected no error, got %v", err)
-    }
-    if key != "testKey" {
-        t.Errorf("expected 'testKey', got %v", key)
-    }
+	headers := http.Header{"Authorization": []string{"ApiKey testKey"}}
+	key, err := GetAPIKey(headers)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if key != "testKey" {
+		t.Errorf("expected 'testKey', got %v", key)
+	}
 
-    // this is a test function to verify that CI is working
+	// this is a test function to verify that CI is working
 }
